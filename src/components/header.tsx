6cc5e14e1e61d3e2d9334e30edea0e8e3de25939
Sohail Zara <sohailzara09@gmail.com>
2025-07-01 10:31:40 +0000

    remove the links in navar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,5 @@
 import Link from 'next/link';
 
-const navLinks = [
-  { href: '/#about', label: 'About' },
-  { href: '/#projects', label: 'Projects' },
-  { href: '/#contact', label: 'Contact' },
-];
-
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -13,17 +7,6 @@ export function Header() {
         <Link href="/#" className="mr-6 flex items-center space-x-2 pl-[10px]">
           <span className="font-bold sm:inline-block font-headline">Noirfolio</span>
         </Link>
-        <nav className="flex items-center gap-6 text-sm">
-          {navLinks.map((link) => (
-            <Link
-              key={link.href}
-              href={link.href}
-              className="transition-colors hover:text-foreground/80 text-foreground/60"
-            >
-              {link.label}
-            </Link>
-          ))}
-        </nav>
       </div>
     </header>
   );
