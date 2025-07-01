57fe5fcd709c3f78bbe388dfbca2091bf53c4cc1
Sohail Zara <sohailzara09@gmail.com>
2025-07-01 09:56:59 +0000

    the header navbar should have padding left 10  px for "noirfolio"

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,7 +10,7 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        <Link href="/#" className="mr-6 flex items-center space-x-2">
+        <Link href="/#" className="mr-6 flex items-center space-x-2 pl-[10px]">
           <span className="font-bold sm:inline-block font-headline">Noirfolio</span>
         </Link>
         <nav className="flex items-center gap-6 text-sm">
