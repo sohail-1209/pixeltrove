8f113a43d4ae363627e836dda972201752af84b0
Sohail Zara <sohailzara09@gmail.com>
2025-07-04 18:39:09 +0000

    ai is not working after hosting

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import 'dotenv/config';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: process.env.GOOGLE_API_KEY})],
   model: 'googleai/gemini-1.5-flash',
 });
