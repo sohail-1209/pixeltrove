f69010f8560ed2d65fca938376be469d4fb5de4a
Sohail Zara <sohailzara09@gmail.com>
2025-07-04 18:55:26 +0000

    AIzaSyDcl9uB2-4EgWiL_PT4C0QO-LHgUqoPJZs

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: 'PASTE_YOUR_GOOGLE_AI_API_KEY_HERE'})],
+  plugins: [googleAI({apiKey: 'AIzaSyDcl9uB2-4EgWiL_PT4C0QO-LHgUqoPJZs'})],
   model: 'googleai/gemini-1.5-flash',
 });
